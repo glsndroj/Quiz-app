480f5c7ef3414e80b458810ee62c3cb3826fdfc8
glsndroj <galsandooorj@gmail.com>
2025-12-01 14:24:22 +0800

    post get hiih

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,34 +1,38 @@
+"use client";
 import ArticleGenerator from "@/components/ArticleGenerator";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  // const [employee, setEmployee] = useState<any[]>([]);
+  const [employee, setEmployee] = useState<any[]>([]);
 
-  // useEffect(() => {
-  //   const getEmployee = async () => {
-  //     try {
-  //       const res = await axios.get("http://localhost:3000/api/employees");
-  //       setEmployee(res.data);
-  //     } catch (error) {
-  //       console.log("error", error);
-  //     }
-  //   };
-  //   getEmployee();
-  // }, []);
+  useEffect(() => {
+    const getEmployee = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/articles");
+        setEmployee(res.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    getEmployee();
+  }, []);
 
   return (
-    <div>
-      <ArticleGenerator />
-    </div>
-    //  <div className="flex gap-5">
-    //   {employee.map((e, i) => {
-    //     return <div key={i}>
-    //       <h1>{e.firstname}</h1>
-    //       <h3>{e.lastname}</h3>
-    //       <p>{e.age}</p>
-    //       <p>{e.gender}</p>
-    //       <p>{e.department}</p>
-    //     </div>
-    //   })}
-    //  </div>
+    <>
+      <div>
+        <ArticleGenerator />
+      </div>
+      <div className="flex gap-5">
+        {employee.map((e, i) => {
+          return (
+            <div key={i}>
+              <h1>{e.title}</h1>
+              <p>{e.content}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
