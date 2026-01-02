48aabca7c2ef1e806deb861e8007e56ba40f9b58
glsndroj <galsandooorj@gmail.com>
2026-01-02 11:35:17 +0800

    Change home path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="w-full h-[60px] flex justify-between items-center px-20 border-b fixed ">
-      <Link href={"http://localhost:3000"}>
+      <Link href={"/"}>
         <h1 className="text-2xl font-semibold cursor-pointer">Quiz app</h1>
       </Link>
 
